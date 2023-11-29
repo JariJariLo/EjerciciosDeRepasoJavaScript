55cd0748d6935a25443e2c493b5d1dc9821fe29e
JariJariLo <146730448+JariJariLo@users.noreply.github.com>
2023-11-29 12:48:08 +0100

    ejers

---
+++ ejer1.js
@@ -0,0 +1,17 @@
+//El programa recibirá un número de comensales, 
+//y después a cuántos de ellos les gusta la tortilla de patata con cebolla y a cuántos sin cebolla. 
+//Una tortilla de patatas lleva 200 gramos de patatas por persona. Por cada kilo de patatas se necesitan 
+//5 huevos y 300 gramos de cebolla. A cada comensal se le reservará media botella de vino. Escribe un script que dado el número de comensales calcule 
+//las cantidades de ingredientes y bebida necesarias.
+const scanner=require("prompt-sync")({sigint:true});
+
+const ncomensales= scanner("Introduce el número de comensales:");
+const numeroCebollistas=ConCebollA(x);
+
+function ConCebollA(x) {
+    const ConCebollistas=scanner("¿Te gusta con cebolla uwu siuu?=>");
+    
+}
+function name() {
+    
+}
